3ec2f0c473318a5435794903613f4f8289ee1ae6
sohm200 <soham.khandagale200@gmail.com>
2024-08-10 14:52:55 +0530

    Add files via upload

---
+++ swap.c
@@ -0,0 +1,15 @@
+#include<cs50.h>
+#include<stdio.h>
+
+int main(void)
+{
+    int a=167;
+    int b=279;
+    a=a+b;
+    b=a-b;
+    a=a-b;
+
+
+    printf("a is %i\n",a);
+    printf("b is %i\n",b);
+}
